961ee49f73b76393ca6493b0dd31be77a2228770
BugHunter-X <askzenai@gmail.com>
2025-01-30 06:22:26 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+function foo(x){
+  if (x == null) {
+    return 0;
+  } else if (x < 0) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+console.log(foo(null)); // Output: 0
+console.log(foo(undefined)); // Output: 1, unexpected
